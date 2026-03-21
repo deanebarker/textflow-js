b84acdad99e443189918354e1357fb583873429a
Deane Barker <deane@deanebarker.net>
2026-03-21 07:52:03 -0500

    This command wasn't require anymore since we added "preserve" to the remove command

--- tests/commands/stripHyperlinks.test.js
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { execute } from "../helpers.js";
-
-test("Strips hyperlinks from HTML", async () => {
-  let command = { name: "strip-hyperlinks" };
-  const result = await execute([command], "<p><a href='https://example.com'>Example</a></p>");
-  expect(result.text).toBe("<p>Example</p>");
-});
